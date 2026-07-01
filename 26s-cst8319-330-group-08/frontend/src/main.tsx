8606cf7f636788f506db84e75e69db1252cfea3a
Avadh Patel <avadhpatel.ca@gmail.com>
2026-06-30 20:15:28 -0400

    Wrap app with theme provider

--- 26s-cst8319-330-group-08/frontend/src/main.tsx
+++ 26s-cst8319-330-group-08/frontend/src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "./components/ThemeProvider";
 import { ToastProvider } from "./components/ToastProvider";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ToastProvider>
-      <App />
-    </ToastProvider>
+    <ThemeProvider>
+      <ToastProvider>
+        <App />
+      </ToastProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
