81d59e218ef90658b0f03c141805d5f6ded277ae
Avadh Patel <avadhpatel.ca@gmail.com>
2026-07-05 14:49:31 -0400

    Load dashboard cleanup styles

--- 26s-cst8319-330-group-08/frontend/src/main.tsx
+++ 26s-cst8319-330-group-08/frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "./components/ThemeProvider";
 import { ToastProvider } from "./components/ToastProvider";
 import "./index.css";
 import "./theme.css";
+import "./dashboard-cleanup.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
