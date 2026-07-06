4ef5a02697507eed82439cd807d1c6374c3c1b11
Avadh Patel <avadhpatel.ca@gmail.com>
2026-07-06 11:03:55 -0400

    Add resource library bookmark endpoints

--- 26s-cst8319-330-group-08/backend/src/routes/resourceRoutes.js
+++ 26s-cst8319-330-group-08/backend/src/routes/resourceRoutes.js
@@ -6,8 +6,11 @@ const resourceController = require("../controllers/resourceController");
 
 router.use(protect);
 
+router.get("/categories/list", resourceController.getResourceCategories);
 router.get("/", resourceController.getResources);
 router.get("/:id", resourceController.getResourceById);
+router.post("/:id/bookmark", resourceController.bookmarkResource);
+router.delete("/:id/bookmark", resourceController.unbookmarkResource);
 router.post("/", resourceController.createResource);
 router.put("/:id", resourceController.updateResource);
 router.delete("/:id", resourceController.deleteResource);
