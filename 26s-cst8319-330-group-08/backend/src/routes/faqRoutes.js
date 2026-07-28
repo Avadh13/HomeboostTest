fbe78be7835da899c04e7fbfd9f4ef2b10fa6734
Avadh Patel <avadhpatel.ca@gmail.com>
2026-07-28 08:55:57 -0400

    Protect FAQ CMS mutations

--- 26s-cst8319-330-group-08/backend/src/routes/faqRoutes.js
+++ 26s-cst8319-330-group-08/backend/src/routes/faqRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const faqController = require("../controllers/faqController");
+const protect = require("../middleware/authMiddleware");
+const { requireAdmin } = require("../middleware/roleMiddleware");
 
 router.get("/", faqController.getFAQs);
-router.post("/", faqController.createFAQ);
-router.put("/:id", faqController.updateFAQ);
-router.delete("/:id", faqController.deleteFAQ);
+router.post("/", protect, requireAdmin, faqController.createFAQ);
+router.put("/:id", protect, requireAdmin, faqController.updateFAQ);
+router.delete("/:id", protect, requireAdmin, faqController.deleteFAQ);
 
 module.exports = router;
